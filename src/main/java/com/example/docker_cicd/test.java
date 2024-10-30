c7560f7bac4d58b62d27d13890383f587b484c8a
전재연 <sunrox1107@gmail.com>
2024-10-30 15:49:53 +0900

    [test] 컨트톨러 생성

---
+++ src/main/java/com/example/docker_cicd/test.java
@@ -0,0 +1,15 @@
+package com.example.docker_cicd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+}
